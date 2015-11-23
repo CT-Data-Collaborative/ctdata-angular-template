d9fc068c957e9ce4016f22a7e93302a4d5c256e1
Sasha Cuerda <sasha@mapconomy.com>
2015-11-23 20:49:15 +0100

    added s3upload and conf file to enable easy upload to an s3 bucket.

---
+++ aws-upload.conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+    credentials:"aws-credentials.json",
+    bucketName:"civichealth.ctdata.org",
+    patterns:[
+        "static/dist/js/*.js",
+        "static/dist/css/*.css",
+        "static/dist/bower_components/**/*.*",
+        "index.html"
+    ]
+}
